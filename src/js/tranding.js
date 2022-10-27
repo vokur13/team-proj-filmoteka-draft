9c53ac57e898d03b887780704c9da229dcb956bb
vokur13 <vokur13@gmail.com>
2022-10-27 20:57:39 +0300

    fixed

--- src/js/tranding.js
+++ src/js/tranding.js
@@ -6,67 +6,20 @@ const refs = {
   trend: document.querySelector('.trend-list'),
 };
 
-console.log(refs.trend);
-
 async function getTrending() {
   try {
     const movies = await API.getMovie();
     const { results } = movies;
+    console.log(results);
+    console.log(results[0].original_title);
     console.log(results[0].poster_path);
-    const markUp = ` <li>
-                                    <div>
-                                            <a href=${results[0].poster_path}>
-                                                    <img src=${results[0].poster_path} alt="name">
-                                            </a>
-                                    </div>
-                                    <div>
-                                            <div>
-                                                    <p>title</p>
-                                            </div>
-                                            <div>
-                                                    <ul>
-                                                            <li>genre</li>
-                                                            <li>genre</li>
-                                                    </ul>
-                                            </div>
-                                            <div>
-                                                    <p>year</p>
-                                            </div>
-                                    </div>
-                            </li>`;
-    //   .map(
-    //     item => ` <li>
-    //                             <div>
-    //                                     <a href=${item.poster_path}>
-    //                                             <img src=${item.poster_path} alt="name">
-    //                                     </a>
-    //                             </div>
-    //                             <div>
-    //                                     <div>
-    //                                             <p>title</p>
-    //                                     </div>
-    //                                     <div>
-    //                                             <ul>
-    //                                                     <li>genre</li>
-    //                                                     <li>genre</li>
-    //                                             </ul>
-    //                                     </div>
-    //                                     <div>
-    //                                             <p>year</p>
-    //                                     </div>
-    //                             </div>
-    //                     </li>`
-    //   )
-    //   .join('');
-    refs.trend.insertAdjacentHTML('beforeend', markUp);
-    //     const genre = results.forEach(element => {
-    //       element.genre_ids;
-    //     });
+    refs.trend.insertAdjacentHTML('beforeend', trendingMarkupTpl(results));
+    const genre = results.forEach(element => {
+      element.genre_ids;
+    });
   } catch (error) {
     console.log(error);
   }
 }
 
 getTrending();
-
-// refs.trend.insertAdjacentHTML('beforeend', trendingMarkupTpl(results));
