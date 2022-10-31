bb0c490c90e48349c8f55eaba52de6fd4dae57e5
vokur13 <vokur13@gmail.com>
2022-10-31 16:26:31 +0200

    firebase under construction

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,27 +1,74 @@
-import { setupWatchedMovies, setupQueueMovies } from './materialize';
-
-// get data
-db.collection('watched_movies')
-  .get()
-  .then(snapshot => {
-    setupWatchedMovies(snapshot.docs);
-  });
-
-db.collection('queue_movies')
-  .get()
-  .then(snapshot => {
-    console.log(snapshot.docs);
-    setupQueueMovies(snapshot.docs);
-  });
+import { setupWatchedMovies, setupQueueMovies, setupUI } from './materialize';
 
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
     console.log('user logged in: ', user);
+    // get data
+    db.collection('watched_movies')
+      .get()
+      .then(snapshot => {
+        setupWatchedMovies(snapshot.docs);
+        setupUI(user);
+      });
+
+    db.collection('queue_movies')
+      .get()
+      .then(snapshot => {
+        setupQueueMovies(snapshot.docs);
+        setupUI(user);
+      });
   } else {
     console.log('user logged out');
+    setupUI(user);
+    setupWatchedMovies([]);
+    setupQueueMovies([]);
   }
 });
+
+// create new guide (add to watched)
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', handleAddToWatched);
+
+function handleAddToWatched(e) {
+  e.preventDefault();
+  db.collection('watched_movies')
+    .add({
+      title: createForm.title.value,
+      content: createForm.content.value,
+    })
+    .then(() => {
+      // close the create modal & reset form
+      const modal = document.querySelector('#modal-create');
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+}
+
+// create new guide (add to queue)
+// const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', handleAddToQueue);
+
+function handleAddToQueue(e) {
+  e.preventDefault();
+  db.collection('queue_movies')
+    .add({
+      title: createForm.title.value,
+      content: createForm.content.value,
+    })
+    .then(() => {
+      // close the create modal & reset form
+      const modal = document.querySelector('#modal-create');
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+}
 
 // signup
 const signupForm = document.querySelector('#signup-form');
--- src/js/materialize.js
+++ src/js/materialize.js
@@ -1,37 +1,61 @@
 // DOM elements
 const watchedMoviesList = document.querySelector('.watched_movies');
 const queueMoviesList = document.querySelector('.queue_movies');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+export const setupUI = user => {
+  if (user) {
+    // toggle user UI elements
+    loggedInLinks.forEach(item => (item.style.display = 'block'));
+    loggedOutLinks.forEach(item => (item.style.display = 'none'));
+  } else {
+    //   toggle user elements
+    loggedInLinks.forEach(item => (item.style.display = 'none'));
+    loggedOutLinks.forEach(item => (item.style.display = 'block'));
+  }
+};
 
 // setup watchedMovies
 export const setupWatchedMovies = data => {
-  let html = '';
-  data.forEach(doc => {
-    const watchedMovies = doc.data();
-    const li = `
+  if (data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const watchedMovies = doc.data();
+      const li = `
           <li>
             <div class="collapsible-header grey lighten-4"> ${watchedMovies.title} </div>
             <div class="collapsible-body white"> ${watchedMovies.content} </div>
           </li>
         `;
-    html += li;
-  });
-  watchedMoviesList.innerHTML = html;
+      html += li;
+    });
+    watchedMoviesList.innerHTML = html;
+  } else {
+    watchedMoviesList.innerHTML =
+      '<h5 class="center-align">Login to view your Library</h5>';
+  }
 };
 
 // setup queueMovies
 export const setupQueueMovies = data => {
-  let html = '';
-  data.forEach(doc => {
-    const queueMovies = doc.data();
-    const li = `
+  if (data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const queueMovies = doc.data();
+      const li = `
           <li>
             <div class="collapsible-header grey lighten-4"> ${queueMovies.title} </div>
             <div class="collapsible-body white"> ${queueMovies.content} </div>
           </li>
         `;
-    html += li;
-  });
-  queueMoviesList.innerHTML = html;
+      html += li;
+    });
+    queueMoviesList.innerHTML = html;
+  } else {
+    queueMoviesList.innerHTML =
+      '<h5 class="center-align">Login to view your Library</h5>';
+  }
 };
 
 // setup materialize components
